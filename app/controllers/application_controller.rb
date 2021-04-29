531cf10c1d3b3e93cfaa26840663dbb29c3bd052
Scott Bobsin <scottbobsin@blueskychurch.com>
2021-04-29 12:18:31 -0700

    nav and dashboard page start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,3 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, 
-                                                            :last_name,
-                                                            :mobile_phone,
-                                                            :address,
-                                                            :city,
-                                                            :state,
-                                                            :zip])
-        devise_parameter_sanitizer.permit(:account_update, keys: [  :first_name, 
-                                                                    :last_name,
-                                                                    :mobile_phone,
-                                                                    :address,
-                                                                    :city,
-                                                                    :state,
-                                                                    :zip])
-    end
+    include DeviseWhitelist
 end
