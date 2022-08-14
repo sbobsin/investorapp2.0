ddc67aa4f7617fcd7a070c078b37d54f613a3662
sbobsin <scottbobsin@gmail.com>
2022-08-14 15:20:20 -0700

    seed and dashboard update

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,10 +19,10 @@ Deal.create!([
 ])
 
 User.create([
-  { first_name: 'Mickey', last_name: 'Mouse', mobile_phone: '000-000-0020', email: 'Mickey@test.com' },
-  { first_name: 'Minnie', last_name: 'Mouse', mobile_phone: '000-000-0030', email: 'Minnie@test.com', address: '1234 any street', city: 'Smalltownusa', zip: '61873' },
-  { first_name: 'Donald', last_name: 'Duck', mobile_phone: '000-000-0040' '1900-01-01', address: '1940 any street', city: 'Smalltownusa', zip: '61873'  },
-  { first_name: 'Scrooge', last_name: 'McDuck', mobile_phone: '000-000-0050' '1900-01-01', address: '2050 any street', city: 'Smalltownusa', zip: '61873' }, 
-  { first_name: 'Gary', last_name: 'Ferguson', mobile_phone: '000-000-0060' '3060 any street', city: 'Smalltownusa', zip: '61873' },
-  { first_name: 'Turd', last_name: 'Ferguson', mobile_phone: '000-000-0070' '1900-01-01', address: '4080 any street', city: 'Smalltownusa', zip: '61873' },
+  { first_name: 'Mickey', last_name: 'Mouse', mobile_phone: '000-000-0020', email: 'Mickey@test.com', password: 'password', password_confirmation: 'password'},
+  { first_name: 'Minnie', last_name: 'Mouse', mobile_phone: '000-000-0030', email: 'Minnie@test.com', address: '1234 any street', city: 'Smalltownusa', zip: '61873',password: 'password', password_confirmation: 'password' },
+  { first_name: 'Donald', last_name: 'Duck', mobile_phone: '000-000-0040', email: 'donald@test.com', address: '1940 any street', city: 'Smalltownusa', zip: '61873',password: 'password', password_confirmation: 'password'  },
+  { first_name: 'Scrooge', last_name: 'McDuck', mobile_phone: '000-000-0050', email: 'Scrooge@test.com', address: '2050 any street', city: 'Smalltownusa', zip: '61873',password: 'password', password_confirmation: 'password' }, 
+  { first_name: 'Gary', last_name: 'Ferguson', mobile_phone: '000-000-0060', email: 'gary@test.com', address: '3060 any street', city: 'Smalltownusa', zip: '61873',password: 'password', password_confirmation: 'password' },
+  { first_name: 'Turd', last_name: 'Ferguson', mobile_phone: '000-000-0070', email: 'turd@test.com', address: '4080 any street', city: 'Smalltownusa', zip: '61873',password: 'password', password_confirmation: 'password' },
 ])
