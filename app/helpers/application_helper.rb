7837dd0804b49f5db2c1f37e341a36124825e4a8
Scott Bobsin <scottbobsin@blueskychurch.com>
2022-08-16 23:43:37 -0700

    merge application helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_user_first_investment_date
+    l(CapitalAccount.where(user_id: current_user.id).first.created_at.to_date, format: :long) rescue nil
+  end 
+
+  def current_user_deals
+    CapitalAccount.where(user_id: current_user.id)
+  end 
 end
